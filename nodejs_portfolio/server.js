60027fb5f2d1108f314454d057557e61ccb06eed
olivier naud <oliviernaud5@gmail.com>
2024-09-08 16:42:03 +0200

    url_front

--- nodejs_portfolio/server.js
+++ nodejs_portfolio/server.js
@@ -42,7 +42,7 @@ app.use(
 //CORS
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.URL_FRONT,
   credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
